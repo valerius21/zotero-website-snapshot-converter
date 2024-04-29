c3c85dc3200567753499eb15745e1314d9f4f451
Valerius Mattfeld <mail@valerius.me>
2024-04-29 16:35:45 +0200

    refactor pdf creation

--- pdf_converter/create_pdf.py
+++ pdf_converter/create_pdf.py
@@ -1,10 +1,17 @@
+from pathlib import Path
+from typing import Optional
+
 from playwright.sync_api import sync_playwright
 
 
-def create_pdf_from_html(html: str, output_path: str):
+def create_pdf_from_html(html: str, output_file_path: str, timeout: Optional[float] = 30.) -> Path:
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.set_content(html)
-        page.pdf(path=output_path)
+        # Wait for the page to load
+        # See https://playwright.dev/dotnet/docs/api/class-page#page-wait-for-load-state
+        page.wait_for_load_state('domcontentloaded', timeout=timeout)
+        page.pdf(path=output_file_path)
         browser.close()
+    return Path(output_file_path)
